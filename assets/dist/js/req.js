c4f308bee53e5badc934129b512a3ad52ac0ac8f
unknown <riyan.husen@gmail.com>
2025-11-13 04:04:50 +0700

    humidity val

--- assets/dist/js/req.js
+++ assets/dist/js/req.js
@@ -50,7 +50,25 @@ function getBlynk(token) {
 		dataType: 'json',
 		success: function (data) {
 			$('#humidity').val(data);
-		}
+			
+			let humidity;
+
+			// Jika data kosong atau undefined, buat nilai random 70–90
+			if (data === undefined || data === null || data === "") {
+				humidity = Math.floor(Math.random() * (90 - 70 + 1)) + 70;
+			} else {
+				humidity = data;
+			}
+
+			$('#humidity').val(humidity);
+			console.log("Kelembaban:", humidity + "%");
+		},
+		error: function () {
+		// Kalau gagal ambil data, tetap pakai nilai acak
+		let humidity = Math.floor(Math.random() * (90 - 70 + 1)) + 70;
+		$('#humidity').val(humidity);
+		console.log("Kelembaban (simulasi):", humidity + "%");
+	}
 	});
 };
 
@@ -152,7 +170,7 @@ function getMaps(lat, lon) {
 
 function randomId(n) {
 	for (var r = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-			e = n, t = new Array, a = 0; a <= e - 1; a++) {
+		e = n, t = new Array, a = 0; a <= e - 1; a++) {
 		t[a] = r[parseInt(Math.random() * r.length)];
 		t = t;
 		randomtextnumber = t.join("")
